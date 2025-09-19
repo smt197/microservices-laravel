ca9da9512b6b02562acd172ff6e752d25d7039b3
smt197 <105449441+smt197@users.noreply.github.com>
2025-09-19 08:42:11 +0000

    Update api.php

--- user-microservice/routes/api.php
+++ user-microservice/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\EmailTestController;
 use Orion\Facades\Orion;
 
 /*
